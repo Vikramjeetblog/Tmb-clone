d54f0c8fca157ef11e78a8833b56cbee4a85d6f5
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2025-11-26 16:01:39 +0530

    Update Popular.js
    updated

--- src/pages/home/popular/Popular.js
+++ src/pages/home/popular/Popular.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
-import ContentWrapper from '../../../component/contentWrapper/ContentWrapper';
-import SwitchTabs from '../../../component/switchTabs/SwitchTabs';
-import useFetch from '../../../hooks/useFetch';
-import Carousel from '../../../component/carousel/Carousel';
+import ContentWrapper from "../../../component/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../component/switchTabs/SwitchTabs";
+import Carousel from "../../../component/carousel/Carousel";
 
-
-const Popular = () => {
+const Popular = ({ movieData, tvData }) => {
     const [endpoint, setEndpoint] = useState("movie");
-
-    const { data, loading } = useFetch(`/${endpoint}/popular`);
 
     const onTabChange = (tab) => {
         setEndpoint(tab === "Movies" ? "movie" : "tv");
     };
 
+    const activeData = endpoint === "movie" ? movieData : tvData;
+
     return (
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">What's Popular</span>
+
                 <SwitchTabs
                     data={["Movies", "TV Shows"]}
                     onTabChange={onTabChange}
                 />
             </ContentWrapper>
+
             <Carousel
-                data={data?.results}
-                loading={loading}
+                data={activeData?.results}
+                loading={!activeData}
                 endpoint={endpoint}
             />
         </div>
